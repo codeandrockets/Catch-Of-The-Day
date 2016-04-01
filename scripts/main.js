d9fbe1300f10d737e507068c8501e1ee59ee0f55
Steven Jay <stevenjasionowicz@gmail.com>
2016-04-01 15:02:12 +1300

    Add main components

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,63 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+//Main App Component
+var App = React.createClass({
+	render : function() {
+		return (
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header />
+				</div>
+				<Order />
+				<Inventory />
+			</div>
+		)
+	}
+});
+
+//Header
+//<Header />
+var Header = React.createClass({
+	render : function() {
+		return (
+			<p>Header</p>
+		)
+	}
+})
+
+//Order
+//<Order />
+var Order = React.createClass({
+	render : function() {
+		return (
+			<p>Order</p>
+		)
+	}
+})
+
+//Inventory
+//<Inventory />
+var Inventory = React.createClass({
+	render : function() {
+		return (
+			<p>Inventory</p>
+		)
+	}
+})
+
 // Store Picker 
 // This will let us make <StorePicker />
-
 var StorePicker = React.createClass({
 	render: function() {
 		return (
-			<h1>Hello</h1>
+			<form className="store-selector">
+				<h2>Please Enter A Store</h2>
+				<input type="text" ref="storeId" required/>
+				<input type="submit" />
+			</form>
 		)
 	} 
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
